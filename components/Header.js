8da54b3a4d979bf05fb686d9133639afba9a8d05
Harsimran Singh <103135191+Harsimran-19@users.noreply.github.com>
2022-08-23 21:41:55 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,12 @@ import { useAuth } from '../context/AuthContext'
 
 
 export default function Header() {
-    const [openModal, setOpenModal] = useState(false)
+
     const { logout } = useAuth()
     return (
         <>
-            {openModal && <Modal setOpenModal={setOpenModal} />}
             <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
                 <h1 className='text-2xl select-none sm:text-1xl'>TODO LIST</h1>
-                {/* <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i> */}
                 <button className="btn border-b py-2 border=solid border-white px-8 bg-cyan-500  hover:bg-purple-500" onClick={()=>{logout()}}>Logout</button>
             </div>
         </>
